49ba1e3af12fa3c18e5f27ff7a626b193085572c
ankit <ankitwalia4u@gmail.com>
2013-07-19 14:00:36 +0530

    added other html files

---
+++ tree_items.js
@@ -0,0 +1,191 @@
+var TREE_ITEMS = [
+	
+['Home', 'links.html',
+		
+['RFIS April 2013 - March 2014', 'ISP-April2013-March2014.pdf'],
+		
+['RF-Focus', 'RF-Focus.pdf'],
+		
+['Program Plan',    'RF-Proposal.pdf'],
+		
+],
+	
+
+['User Profile', null,
+		
+	['Andhra Pradesh', '',
+	['Block Wise Crop Details', 'userprofile/AP-Cropwise.pdf',],
+	['Block Wise Fisheries Details', 'userprofile/AP-Fish.pdf',],
+	['Block Wise Live Stock Details', 'userprofile/AP-Livestock.pdf',],
+	
+			
+	['Profile', 'userprofile/ap-profile.html',]],
+
+	['Puducherry', '',
+	['Block wise Crop Details', 'userprofile/PY-Cropwise.pdf',],
+	['Block wise Fishermen Details', 'userprofile/PY-Fish.pdf',],
+	['Block wise Livestock Details', 'userprofile/PY-Livestock.pdf',],
+	
+			
+	['Profile', 'userprofile/py-profile.html',]],
+
+	['Tamil Nadu', '',
+	['Block wise Crop Details', 'userprofile/TN-Cropwise.pdf',],
+	['Block wise Fishermen Details', 'userprofile/TN-Fish.pdf',],
+	['Block wise Livestock Details', 'userprofile/TN-Livestock.pdf',],
+	
+			
+	['Profile', 'userprofile/tn-profile.html',]]],
+
+['Video Gallery', null,
+	
+	
+	['Andhra Pradesh', '',
+		['Agriculture', '',
+			['Aug-Dec-2012', 'gallery/AP-AG-gallery-Year-2012.html',],
+			
+		],
+		['Fisheries', '',
+			['Aug-Dec-2012', 'gallery/TN-FH-gallery-Year-2012.html',],
+			
+		],
+		['Live Stock', '',
+			['Aug-Dec-2012', 'gallery/TN-LS-gallery-Year-2012.html',],
+			
+		]],
+	
+
+	['Puducherry', '',
+		
+		['Fisheries', '',
+			['Aug-Dec-2012', 'gallery/PY-FH-gallery-Year-2012.html',],
+			
+		]],
+		
+
+	['Tamil Nadu', '',
+		['Agriculture', '',
+			['Aug-Dec-2012', 'gallery/TN-AG-gallery-Year-2012.html',],
+			
+		],
+		['Fisheries', '',
+			['Aug-Dec-2012', 'gallery/TN-FH-gallery-Year-2012.html',],
+			
+		],
+		['Live Stock', '',
+			['Aug-Dec-2012', 'gallery/TN-LS-gallery-Year-2012.html',],
+			
+		]
+	]
+	
+],
+
+
+['Mobile Services', null,
+		
+	['Andhra Pradesh', '',
+	['Agriculture', 'Mobile-advisories/AP-SMS.html',],
+	['Fisheries', 'Mobile-advisories/AP-FS-SMS.html',],
+	
+	
+	],
+
+	
+	['Tamil Nadu & Puducherry', '',
+	['Agriculture', 'TN-SMS.html',],
+	['Fisheries', 'Fish-TN-RF.html',],
+	
+
+],],
+
+['Field Programme', null,
+		
+	['Andhra Pradesh', '',
+	['Audio Conference', 'confer/AP-audio.html',],
+	['Cable TV', 'confer/AP-cable.html',],
+	['Training', 'confer/AP-Train.html',],
+	['Awareness', 'confer/AP-Aware.html',],
+	
+	],
+
+	['Puducherry', '',
+	['Audio Conference', 'confer/PY-audio.html',],
+	['Cable TV', 'confer/PY-cable.html',],
+	['Training', 'confer/PY-Train.html',],
+	['Awareness', 'confer/PY-Aware.html',],
+	
+	
+	],
+	
+	['Tamil Nadu', '',
+	['Audio Conference', 'confer/TN-audio.html',],
+	['Cable TV', 'confer/TN-cable.html',],
+	['Training', 'confer/TN-Train.html',],
+	['Awareness', 'confer/TN-Aware.html',],
+	
+
+],],
+
+
+['RF- Advisories', null,
+		
+	
+
+['Tamil Nadu', '',
+	['Agriculture', '',
+
+	['Pudukkottai', '',
+		['Jan 2013', 'Agro-advisories/pdk-advisory-jan2013.html',],
+		['Feb 2013', 'Agro-advisories/pdk-advisory-feb2013.html',],
+	],
+
+	['Thanjavur', '',
+		['Jan 2013', 'Agro-advisories/tj-advisory-jan2013.html',],
+		['Feb 2013', 'Agro-advisories/tj-advisory-feb2013.html',],
+	],
+	['Dindigul', '',
+		['Jan 2013', 'Agro-advisories/dj-advisory-jan2013.html',],
+		['Feb 2013', 'Agro-advisories/dj-advisory-feb2013.html',],
+	],
+	['Ramanathapuram', '',
+		['Feb 2013', 'Agro-advisories/rmd-advisory-feb2013.html',],
+	],
+	],
+
+	['Fisheries', '',
+
+	
+	['Nagapattinam', '',
+		['Year 2012', 'Fish-advisories/ngt-advisory-aug-dec2012.html',],
+		['Jan 2013', 'Fish-advisories/ngt-advisory-jan2013.html',],
+	],
+	['Ramanathapuram', '',
+		['Year 2012', 'Fish-advisories/rmd-advisory-aug-dec2012.html',],
+		['Jan 2013', 'Fish-advisories/rmd-advisory-jan2013.html',],
+	],
+	
+	]],
+
+['Andhra Pradesh', '',
+	['Agriculture', '',
+
+	['Kakinada', '',
+		['Jan 2013', 'Agro-advisories/pdk-advisory-jan2013.html',],
+		['Feb 2013', 'Agro-advisories/pdk-advisory-feb2013.html',],
+	],
+
+	
+	],
+
+	['Fisheries', '',
+
+	
+	['Kakinada', '',
+		['Jan 2013', 'Fish-advisories/ap-advisory-jan2013.html',],
+	],
+		
+	],
+			
+	]],
+
+];
